cd3f8295c51a66fd706ebb794cd889b4feb20a48
weibin <372618291@qq.com>
2022-07-05 16:54:37 +0800

    chore: 同步gitee

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,16 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 拷贝目录和文件
+cp -r ../../../.github ./
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:wb1128/blog.git master:gh-pages
+# 同步上传gitee
+# git push -f git@gitee.com:wwebin/blog.git master:gh-pages
 
 cd -
